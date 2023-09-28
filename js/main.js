65b0cedf47b497571d98751ed383fb38b241dd36
Captain Mylo <imylomylo@gmail.com>
2023-09-28 10:06:38 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,25 @@
-const config = require('./config');
+const verusRpc = require('./verus-rpc');
+const transactionHash = 'your-transaction-hash-here';
+const address = 'your-address-here';
 
-// Now you can access the variables like config.VERUS, config.VERUSARGS, etc.
+// Usage examples for transaction-related methods
+verusRpc.getRawTransaction(transactionHash)
+  .then((rawTransaction) => {
+    console.log('Raw Transaction:', rawTransaction);
+    return verusRpc.decodeRawTransaction(rawTransaction);
+  })
+  .then((decodedTransaction) => {
+    console.log('Decoded Transaction:', decodedTransaction);
+  })
+  .catch((error) => {
+    console.error('Error:', error);
+  });
+
+// Usage example for getAddressUtxos
+verusRpc.getAddressUtxos(address)
+  .then((utxos) => {
+    console.log('Address UTXOs:', utxos);
+  })
+  .catch((error) => {
+    console.error('Error:', error);
+  });
